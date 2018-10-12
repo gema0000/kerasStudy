3d7101397bf86a5e9e92874aa6a8bc140e53c364
gema <gema0000@naver.com>
2018-10-13 02:55:03 +0900

    Create study20181006_base.py
    X값으로 1~5, Y값으로 1~5가 주어젓을때, 단순선형회귀로 W값을 구한다.

---
+++ study20181006_base.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+
+#1. 문제 정의하기
+from keras.preprocessing import sequence
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Activation, Embedding, LSTM
+# from keras.layers import Conv1D, MaxPooling1D
+# from keras.datasets import imdb
+# from keras.utils import np_utils
+# from keras.optimizers import SGD, RMSprop, Adam
+# from keras.layers.convolutional import Conv2D, MaxPooling2D, Conv1D, MaxPooling1D
+# from keras.layers.core import Dense, Dropout, Activation, Flatten
+# from keras import backend as K
+import numpy as np
+# import pandas as pd
+# import keras
+# import tensorflow as tf
+
+np.random.seed(777)
+
+# 데이터 생성
+x = np.array([1,2,3,4,5])   # (5,)
+y = np.array([1,2,4,7,5])   # (5,)
+
+print(type(x))
+print(x.shape)
+
+# 훈련과 검증 분리
+x_train = x[:3]  # 1,2,3
+y_train = y[:3]
+x_test = x[3:]   # 4,5
+y_test = y[3:]
+
+# print(x_train)
+print(x_train.shape)
+print(x_test.shape)
+
+
+# 모델 구성하기
+
+model = Sequential()
+model.add(Dense(7, input_dim=1, activation='relu'))
+model.add(Dense(3))
+model.add(Dense(1, activation='relu'))
+
+
+# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+
+model.summary()
+
+
+model.fit(x_train, y_train, epochs=1000, batch_size=1, validation_data=(x_test, y_test))
+
+loss, acc = model.evaluate(x_test, y_test, batch_size=1)
+
+print(loss, acc)
+
+print(model.predict(x_test))
+
+
