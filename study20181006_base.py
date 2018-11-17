a409ea6633f84b37ec0ecacce031478dcb20543b
gema0000 <gema0000@naver.com>
2018-11-17 13:59:02 +0900

    20181107 윤영선 소스수정

--- study20181006_base.py
+++ study20181006_base.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+# 20181117 최종수정
+
 #1. 문제 정의하기
 from keras.preprocessing import sequence
 from keras.models import Sequential, Model
@@ -13,17 +15,24 @@ import numpy as np
 np.random.seed(777)
 
 # 데이터 생성
-x = np.array([1,2,3,4,5])   # (5,)
-y = np.array([1,2,4,7,5])   # (5,)
+x = np.array([1,2,3,4,5,6,7,8,9,10,11,12])   # (5,)
+y = np.array([2,4,6,8,10,12,14,16,18,20,22,24])   # (5,)
 
 print(type(x))
 print(x.shape)
 
+
+
+
+
+
+
+
 # 훈련과 검증 분리
-x_train = x[:3]  # 1,2,3
-y_train = y[:3]
-x_test = x[3:]   # 4,5
-y_test = y[3:]
+x_train = x[:7]  # 1,2,3,4,5,6,7
+y_train = y[:7]
+x_test = x[7:]   # 8, 9, 10, 11, 12
+y_test = y[7:]
 
 # print(x_train)
 print(x_train.shape)
@@ -33,7 +42,7 @@ print(x_test.shape)
 # 모델 구성하기
 
 model = Sequential()
-model.add(Dense(7, input_dim=1, activation='relu'))
+model.add(Dense(5, input_dim=1, activation='relu'))     # 7일경우보다 5일경우 predict가 더 잘나옴.
 model.add(Dense(3))
 model.add(Dense(1, activation='relu'))
 
@@ -44,16 +53,18 @@ model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
 
-model.fit(x_train, y_train, epochs=1000, batch_size=1, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, epochs= 500, batch_size=1, validation_data=(x_test, y_test))
 
 loss, acc = model.evaluate(x_test, y_test, batch_size=1)
 
-print(loss, acc)
+print("loss : ", loss)
+print("acc : ", acc)
 
 print(model.predict(x_test))
 
-# 깃허브 테스트용 주석입니다.
-# 깃허브 테스트용 주석2입니다.
+
+
+
 
 '''
 Epoch 1000/1000
--- study20181013_rnn.py
+++ study20181013_rnn.py
@@ -10,7 +10,8 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
 # from keras.utils import np_utils
 
-a = np.array([11,12,13,14,15,16,17,18,19,20])
+# a = np.array([11,12,13,14,15,16,17,18,19,20])
+a = np.array(range(1,100))
 
 def split_5(seq, window_size):  # 데이터를 5개씩 자르기용.    # 입력이 5이고 5개씩 자르기
     aaa = []
@@ -24,19 +25,53 @@ def split_5(seq, window_size):  # 데이터를 5개씩 자르기용.    # 입력
 dataset = split_5(a, window_size=5)     # 5씩 잘랏으니 (5, 6)가 된다. // window_size+1 만큼씩 잘라진다.
 print("===========================")
 print(dataset)
-print(dataset.shape)
+print(dataset.shape) #15, 5
+
+from sklearn.model_selection import train_test_split
+
+# X_train, X_test, y_train, y_test = train_test_split(
+#     cancer.data, cancer.target, stratify = cancer.target, random_state=66
+# )
 
 
 #입력과 출력을 분리시키기  5개와 1개로
 
-x_train = dataset[:,0:4]
-y_train = dataset[:,4]
+x_data = dataset[:,0:4] 
+y_data = dataset[:,4]   
 
-x_train = np.reshape(x_train, (6, 4, 1))
+# 표준화
+# from sklearn.preprocessing import MinMaxScaler
+# scaler = MinMaxScaler()
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(x_data)
+x_data2 = scaler.transform(x_data)
+
+print(x_data.shape)     # 95, 4
+x_data = np.reshape(x_data2, (95, 4, 1))
+print(x_data2.shape)
 
 
-print(x_train.shape)
-print(y_train.shape)
+x_train, x_test, y_train, y_test = train_test_split(
+    x_data, y_data, random_state=66
+)
+
+print(x_train)
+print(y_train)          # 23, 15, ...
+
+print(x_train.shape)    # 11, 4, 1
+print(y_train.shape)    # 11,
+print(x_test.shape)     # 4,4,1
+
+print("스케일 조정 후 특성별 최소값:\n {}".format(x_train.min(axis=0)))
+print("스케일 조정 후 특성별 최대값:\n {}".format(x_train.max(axis=0)))
+print("스케일 조정 후 특성별 최소값:\n {}".format(x_test.min(axis=0)))
+print("스케일 조정 후 특성별 최대값:\n {}".format(x_test.max(axis=0)))
+
+
+#전처리없이 진행
+x_train2 = x_train
+x_test2 = x_test
 
 
 # 모델 구성하기
@@ -51,17 +86,23 @@ model.summary()
 
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=500, batch_size=1, verbose=2)
+# model.fit(x_train, y_train, epochs=500, batch_size=1, verbose=2)
+model.fit(x_train2, y_train, epochs= 200, batch_size=1, validation_data=(x_test2, y_test), verbose=2)
 
-loss, acc = model.evaluate(x_train, y_train)
+loss, acc = model.evaluate(x_test2, y_test)
 
+print("loss : ", loss)
+print("acc : ", acc)
 
-x_test = np.array([[[101],[102],[103],[104]], [[201],[202],[203],[204]]])
-
-y_predict = model.predict(x_test)
-
-
+y_predict = model.predict(x_test2)
 print(y_predict)
+
+
+x_test2 = np.array([[[101],[102],[103],[104]], [[201],[202],[203],[204]]])
+y_predict2 = model.predict(x_test2)
+print(y_predict2)
+
+
 
 '''
 Epoch 500/500
